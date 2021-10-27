a0ed16d0d6bc35e6c8cb205e03fb8ef189d84ebd
Eugene Baichuk <eugene.baichuk@yandex.by>
2021-10-27 21:41:41 +0200

    Modal added

--- dist/js/script.js
+++ dist/js/script.js
@@ -85,4 +85,49 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   setClock('.timer', deadline); //подставляем класс таймера на странице и нужного дедлайна
 
+  // Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('.modal__close');
+
+  function closeModal() {
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+
+  function openModal() {
+    modal.classList.add('show');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+
+
+  modalTrigger.forEach((item) => {
+    item.addEventListener('click', openModal);
+  });
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 20000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+  window.addEventListener('scroll', showModalByScroll);
 });
